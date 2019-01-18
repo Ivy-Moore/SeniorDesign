96256cc731859fea411f3c3b59eefc22156d5acc
cusengan <lordvinh1@gmail.com>
2019-01-18 16:32:20 -0500

    Item List View created and launch camera activity

--- app/src/main/java/com/seniordesign/MainActivity.java
+++ app/src/main/java/com/seniordesign/MainActivity.java
@@ -3,15 +3,36 @@ package com.seniordesign;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button cameraButton;
+    private Button itemListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, ItemListActivity.class);
-        startActivity(intent);
-                                          
+
+        cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        itemListButton = findViewById(R.id.itemListButton);
+        itemListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ItemListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
