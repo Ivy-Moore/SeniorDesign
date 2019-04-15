2b86d869a5e879bc63f3bacf1ac18c049a8d47f0
William Truong <lordvinh1@gmail.com>
2019-04-14 23:54:46 -0500

    Modify server

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, request
 import os
 from googleapiclient.discovery import build
@@ -9,10 +11,19 @@ api_key = 'AIzaSyCnCwf-1gIzU0NjwtEL5ItILqlGlH659XI'
 cse_id = '013673076675552337852:qdyrlcmlyli'
 
 
+class Item:
+
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
+
+    def __repr__(self):
+        return "title:{0}~link:{1}".format(self.title, self.link)
+
+
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build('customsearch', 'v1', developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()['items']
-
     items = []
     response = []
     for item in res:
@@ -24,30 +35,30 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     return ''.join(response)
 
 
-@app.route('/test', methods = ['POST', 'GET'])
+@app.route('/picture', methods = ['POST', 'GET'])
 def test():
-    image_path = "clothes.jpg"
+    image_path = "demofile2.jpg"
     result_path = 'results.txt'
     graph_path = 'tf_files/retrained_graph.pb'
 
     file = request.files['pic']
     f = open(image_path, "wb")
-    for line in file: 
+    for line in file:
         f.write(line)
     f.close()
 
-    # command = 'python -m scripts.label_image --graph=' + graph_path + ' --image=' + image_path + ' > ' + result_path
-    # os.system(command)
-    # result_file = open(result_path, 'r')
-    # prediction = str(result_file.read())
-    return str(file.read())
+    command = 'python -m scripts.label_image --graph=' + graph_path + ' --image=' + image_path + ' > ' + result_path
+    os.system(command)
+    result_file = open(result_path, 'r')
+    prediction = str(result_file.read())
+    return prediction
 
 
 
 @app.route('/getResults', methods = ['POST', 'GET'])
 def getResults():
     clothes = request.form.get('choice')
-    return google_search(clothes, api_key, cse_id, num=3)
+    return google_search(clothes, api_key, cse_id, num=10)
 
 
 app.run()
