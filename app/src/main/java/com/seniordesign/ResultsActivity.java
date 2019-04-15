6b443771e006f26f0d74e56515baacf4bc43b7f1
William Truong <lordvinh1@gmail.com>
2019-04-14 19:36:45 -0500

    Create item class

--- app/src/main/java/com/seniordesign/ResultsActivity.java
+++ app/src/main/java/com/seniordesign/ResultsActivity.java
@@ -6,7 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultsActivity extends AppCompatActivity {
+
+    private class Item {
+        String title;
+        String link;
+
+        public Item(String title, String link) {
+            this.title = title;
+            this.link = link;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Title - %s, Link - %s", title, link);
+        }
+    }
 
     private TextView httpString;
 
@@ -20,6 +38,19 @@ public class ResultsActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String httpStr = bundle.getString("http_string");
+        String[] httpStrSplit = httpStr.split("\n");
+        List<Item> items = new ArrayList<>();
+
+        for (String item : httpStrSplit) {
+            String title = item.split(",")[0].split(":")[1];
+            String link = item.split(",")[1].split(":")[1];
+            items.add(new Item(title, link));
+        }
+
+        for (Item item : items) {
+            System.out.println(item);
+        }
+
         httpString.setText(httpStr);
 
     }
